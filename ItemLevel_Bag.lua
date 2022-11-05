20b7e4d7719c8079e3d1b0673d63e4733c2b7fd4
enderneko <enderneko-dev@outlook.com>
2022-11-06 02:40:54 +0800

    Add support for Inventorian

--- ItemLevel_Bag.lua
+++ ItemLevel_Bag.lua
@@ -7,6 +7,14 @@ local DAI = select(2, ...)
 local bagFontStrings = {}
 DAI.bagFontStrings = bagFontStrings
 
+function DAI:UpdateAllBags(show)
+    if not show then
+        for _, s in pairs(bagFontStrings) do
+            s:SetText("")
+        end
+    end
+end
+
 local function GetButtonFontString(button)
     local name = button:GetName()
     if not bagFontStrings[name] then
@@ -56,58 +64,73 @@ local function UpdateButton(button, bag, slot)
     buttonFontString:SetText(s)
 end
 
--- blizzard bags
--- hooksecurefunc("ContainerFrame_Update", function(frame)
---     for i = 1, frame.size do
---         local itemButton  = _G[frame:GetName() .. "Item" .. i]
---         if itemButton then
---             UpdateButton(itemButton, frame:GetID(), itemButton:GetID())
---         end
---     end
--- end)
---! Interface\FrameXML\ContainerFrame.lua - ContainerFrameMixin:UpdateItems()
-local function UpdateItems(frame)
-    -- print(frame:GetName())
-    for _, itemButton in frame:EnumerateValidItems() do
-        UpdateButton(itemButton, itemButton:GetBagID(), itemButton:GetID())
-    end
-end
-for _, frame in ipairs(UIParent.ContainerFrames) do
-    hooksecurefunc(frame, "UpdateItems", UpdateItems)
-end
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", function()
+    frame:UnregisterAllEvents()
 
--- blizzard bank bags
-hooksecurefunc("BankFrameItemButton_Update", function(button)
-    if not button.isBag then
-        UpdateButton(button, -1, button:GetID())
+    if IsAddOnLoaded("Bagnon") then -- Bagnon
+        hooksecurefunc(Bagnon.Item, "Update", function(self)
+            UpdateButton(self, self:GetBag(), self:GetID())
+        end)
+        
+    elseif IsAddOnLoaded("Baggins") then -- Baggins
+        hooksecurefunc(Baggins, "UpdateItemButton", function(baggins, bagframe, button, bag, slot)
+            UpdateButton(button, bag, slot)
+        end)
+
+    elseif IsAddOnLoaded("Combuctor") then -- Combuctor
+        hooksecurefunc(Combuctor.Item, "Update", function(self)
+            UpdateButton(self, self:GetBag(), self:GetID())
+        end)
+
+    elseif IsAddOnLoaded("Inventorian") then -- Inventorian -- stolen from Simple Item Level
+        local Inventorian = LibStub("AceAddon-3.0", true):GetAddon("Inventorian", true)
+        local function ToIndex(bag, slot)
+            return (bag < 0 and bag * 100 - slot) or (bag * 100 + slot)
+        end
+        local function UpdateSlot(self, bag, slot)
+            if not self.items[ToIndex(bag, slot)] then return end
+            UpdateButton(self.items[ToIndex(bag, slot)], bag, slot)
+        end
+        local function hookInventorian()
+            hooksecurefunc(Inventorian.bag.itemContainer, "UpdateSlot", UpdateSlot)
+            hooksecurefunc(Inventorian.bank.itemContainer, "UpdateSlot", UpdateSlot)
+        end
+        if Inventorian.bag then
+            hookInventorian()
+        else
+            hooksecurefunc(Inventorian, "OnEnable", function()
+                hookInventorian()
+            end)
+        end
+
+    else -- blizzard bags
+        -- hooksecurefunc("ContainerFrame_Update", function(frame)
+        --     for i = 1, frame.size do
+        --         local itemButton  = _G[frame:GetName() .. "Item" .. i]
+        --         if itemButton then
+        --             UpdateButton(itemButton, frame:GetID(), itemButton:GetID())
+        --         end
+        --     end
+        -- end)
+        --! Interface\FrameXML\ContainerFrame.lua - ContainerFrameMixin:UpdateItems()
+        local function UpdateItems(frame)
+            -- print(frame:GetName())
+            for _, itemButton in frame:EnumerateValidItems() do
+                UpdateButton(itemButton, itemButton:GetBagID(), itemButton:GetID())
+            end
+        end
+        for _, frame in ipairs(UIParent.ContainerFrames) do
+            hooksecurefunc(frame, "UpdateItems", UpdateItems)
+        end
+        hooksecurefunc(ContainerFrameCombinedBags, "UpdateItems", UpdateItems)
+
+        -- blizzard bank bags
+        hooksecurefunc("BankFrameItemButton_Update", function(button)
+            if not button.isBag then
+                UpdateButton(button, -1, button:GetID())
+            end
+        end)
     end
 end)
-
--- bagnon
-if IsAddOnLoaded("Bagnon") then
-    hooksecurefunc(Bagnon.Item, "Update", function(self)
-        UpdateButton(self, self:GetBag(), self:GetID())
-    end)
-end
-
--- Combuctor
-if IsAddOnLoaded("Combuctor") then
-    hooksecurefunc(Combuctor.Item, "Update", function(self)
-        UpdateButton(self, self:GetBag(), self:GetID())
-    end)
-end
-
--- Baggins
-if IsAddOnLoaded("Baggins") then
-    hooksecurefunc(Baggins, "UpdateItemButton", function(baggins, bagframe, button, bag, slot)
-        UpdateButton(button, bag, slot)
-    end)
-end
-
-function DAI:UpdateAllBags(show)
-    if not show then
-        for _, s in pairs(bagFontStrings) do
-            s:SetText("")
-        end
-    end
-end
